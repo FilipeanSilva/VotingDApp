99d46fd7e4e4bc5c7d2c057d15d9e5103457597f
Dr33-Dell <fanssilva06@gmail.com>
2024-10-26 19:45:40 +0200

    Base working

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.27",
+  solidity: '0.8.11',
+  defaultNetwork: 'localhost',
+  networks: {
+    hardhat: {},
+    sampoia: {
+      url: process.env.ARBITRUM_SAMPOIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    localhost: {
+      url: process.env.localhost_url,
+      accounts: [process.env.pk_localhost],
+    },
+  },
 };
