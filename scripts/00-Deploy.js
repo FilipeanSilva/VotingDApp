96ff7414fe7a763a1a8bf5742762233c01dd996c
Dr33-Dell <fanssilva06@gmail.com>
2024-11-10 10:03:28 +0100

    updated logs 00-Deploy.js

--- scripts/00-Deploy.js
+++ scripts/00-Deploy.js
@@ -15,15 +15,23 @@ async function main() {
     const votingContract = await Voting.deploy();
     await votingContract.deployed();
 
-    console.log('Voting contract deployed to:', votingContract.address);
+    console.log(
+      `Voting contract deployed successfully.\n` +
+        `Network Details:\n` +
+        `- Network Name: ${hre.network.name}\n` +
+        `- Network URL: ${hre.network.config.url}\n` +
+        `- Contract Address: ${votingContract.address}`
+    );
 
     // Update the .env file with the new contract address
     updateEnvFile('CONTRACT_ADDRESS', votingContract.address);
   } catch (e) {
     if (e.code == 'NETWORK_ERROR') {
       console.error(
-        '-- Failed to deploy Voting contract due to network error. Verify .env file or network setup. --\n\n' +
-          e.message
+        `-- Failed to deploy Voting contract due to network error. Verify .env file or/and network setup.--\n
+        Network URL: ${hre.network.config?.url || 'Not available'}
+        Network Name: ${hre.network.config?.name || 'Not available'} \n
+        ${e.message}`
       );
     }
     console.error('Failed to deploy Voting contract:', e);
@@ -49,7 +57,7 @@ function updateEnvFile(key, value) {
 
     // Write changes to .env file
     fs.writeFileSync(envPath, updatedEnvContents.trim()); // Trim to avoid leading newlines
-    console.log(`Updated ${key} in .env file.`);
+    console.log(`Updated ${key} in .env file`);
   } catch (error) {
     console.error(`Failed to update .env file:`, error);
   }
